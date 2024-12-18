0d92ddc40e2c7716bbb0163c39a26206ed74944f
JRSmile <jrsmile@users.noreply.github.com>
2024-12-19 00:19:47 +0100

    change bins to bin to be able to extract to .local

--- fetch.sh
+++ fetch.sh
@@ -2,23 +2,23 @@
 wget https://github.com/devmatteini/dra/releases/download/0.7.0/dra-0.7.0-x86_64-unknown-linux-musl.tar.gz
 tar -xzf dra-0.7.0-x86_64-unknown-linux-musl.tar.gz
 chmod +x ./dra-0.7.0-x86_64-unknown-linux-musl/dra
-mkdir /tmp/bins
-./dra-0.7.0-x86_64-unknown-linux-musl/dra download -a -i -o /tmp/bins/ devmatteini/dra
-chmod +x /tmp/bins/dra
-/tmp/bins/dra download -a -i -o /tmp/bins/ junegunn/fzf
-/tmp/bins/dra download -a -i -o /tmp/bins/ ogham/exa
-/tmp/bins/dra download -a -i -o /tmp/bins/ pvolok/mprocs
-/tmp/bins/dra download -a -i -o /tmp/bins/ BurntSushi/ripgrep
-/tmp/bins/dra download -a -i -o /tmp/bins/ sxyazi/yazi
-/tmp/bins/dra download -a -i -o /tmp/bins/ F1bonacc1/process-compose
-/tmp/bins/dra download -a -i -o /tmp/bins/ orf/gping
-/tmp/bins/dra download -a -i -o /tmp/bins/ hengyoush/kyanos
-/tmp/bins/dra download -a -i -o /tmp/bins/ wtfutil/wtf
-/tmp/bins/dra download -a -i -o /tmp/bins/ jesseduffield/lazygit
-/tmp/bins/dra download -a -i -o /tmp/bins/ jorgerojas26/lazysql
-/tmp/bins/dra download -a -i -o /tmp/bins/ jesseduffield/lazydocker
-/tmp/bins/dra download -a -i -o /tmp/bins/ darrenburns/posting
-/tmp/bins/dra download -a -i -o /tmp/bins/ helix-editor/helix
-/tmp/bins/dra download -a -i -o /tmp/bins/ charles-001/dolphie
-/tmp/bins/dra download -a -i -o /tmp/bins/ tulir/gomuks
-ls -llah /tmp/bins/
+mkdir /tmp/bin
+./dra-0.7.0-x86_64-unknown-linux-musl/dra download -a -i -o /tmp/bin/ devmatteini/dra
+chmod +x /tmp/bin/dra
+/tmp/bin/dra download -a -i -o /tmp/bin/ junegunn/fzf
+/tmp/bin/dra download -a -i -o /tmp/bin/ ogham/exa
+/tmp/bin/dra download -a -i -o /tmp/bin/ pvolok/mprocs
+/tmp/bin/dra download -a -i -o /tmp/bin/ BurntSushi/ripgrep
+/tmp/bin/dra download -a -i -o /tmp/bin/ sxyazi/yazi
+/tmp/bin/dra download -a -i -o /tmp/bin/ F1bonacc1/process-compose
+/tmp/bin/dra download -a -i -o /tmp/bin/ orf/gping
+/tmp/bin/dra download -a -i -o /tmp/bin/ hengyoush/kyanos
+/tmp/bin/dra download -a -i -o /tmp/bin/ wtfutil/wtf
+/tmp/bin/dra download -a -i -o /tmp/bin/ jesseduffield/lazygit
+/tmp/bin/dra download -a -i -o /tmp/bin/ jorgerojas26/lazysql
+/tmp/bin/dra download -a -i -o /tmp/bin/ jesseduffield/lazydocker
+/tmp/bin/dra download -a -i -o /tmp/bin/ darrenburns/posting
+/tmp/bin/dra download -a -i -o /tmp/bin/ helix-editor/helix
+/tmp/bin/dra download -a -i -o /tmp/bin/ charles-001/dolphie
+/tmp/bin/dra download -a -i -o /tmp/bin/ tulir/gomuks
+ls -llah /tmp/bin/
